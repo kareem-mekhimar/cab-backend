da50231cc1c3a7837488c4615eafda57244ca3ca
km <k.i.m.oo@hotmail.com>
2017-12-28 08:41:02 +0200

    asd

--- events_handler.js
+++ events_handler.js
@@ -344,7 +344,7 @@ module.exports = (io) => {
         socket.on("driver:afterPassCancel", () => {
             // socket.leave(socket.room);
             // socket.room = socket.phone;
-
+   
             // console.log("Joining room "+socket.room) ;
 
             // socket.join(socket.room);
