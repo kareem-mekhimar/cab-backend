282b3531b2e9804738c660caca8120ba53819007
km <k.i.m.oo@hotmail.com>
2018-02-28 10:43:52 +0200

    as

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -1,14 +1,14 @@
 const Driver = require("../models/driver");
-const Period = require("../models/period") ;
-const Trip = require("../models/trip") ;
+const Period = require("../models/period");
+const Trip = require("../models/trip");
 const redisClient = require("../redis");
 const userController = require("./user_controller");
-const { ApiResponse } = require("../helpers/ApiResponse") ;
-const getEta  = require("../utils").getEta;
+const { ApiResponse } = require("../helpers/ApiResponse");
+const getEta = require("../utils").getEta;
 var path = require("path");
 var multer = require("multer");
-const moment = require("moment") ;
-const DailyReport = require("../models/daily_report") ;
+const moment = require("moment");
+const DailyReport = require("../models/daily_report");
 
 
 var upload = multer({
@@ -133,30 +133,33 @@ module.exports = {
 
         Driver.update({ _id: id }, { status: "HIRED" }).then(driver => {
 
-            let nowMoment = moment().startOf('day') ;
-            let nowDate = nowMoment.toDate() ;
+            let nowMoment = moment().startOf('day');
+            let nowDate = nowMoment.toDate();
 
-            let endDate = nowMoment.clone().add(14, 'days').toDate() ;
-            let period = new Period({ 
-               startDate: nowDate,
-               endDate: endDate
-             }) ;
+            let endDate = nowMoment.clone().add(14, 'days').toDate();
+            let period = new Period({
+                startDate: nowDate,
+                endDate: endDate
+            });
 
-             period.save().then(period => {
+            period.save().then(period => {
 
-                for(var i = 1 ; i <= 14 ; i++){
+                for (var i = 1; i <= 14; i++) {
                     let report = new DailyReport({
-                        dayDate: nowMoment.clone().add(i, 'days').toDate() ,
+                        dayDate: i == 1 ? nowMoment.toDate() : nowMoment.clone().add(i, 'days').toDate(),
                         period: period._id,
-                        driver: driver._id
+                        driver: id
                     });
 
-                    report.save() ;
+                    report.save();
                 }
 
-                driver.currentPeriod = period._id ;
-                driver.save() ;
-             }) ;
+                Driver.findById(id).then(d => {
+                    d.currentPeriod = period._id;
+                    d.save();
+                })
+
+            });
 
             userController.verifyDriverUser(id, password).then(user => {
                 res.status(200).end();
@@ -169,9 +172,9 @@ module.exports = {
                 console.log(err);
                 return res.status(400).send("an Error occured")
             }
-            let appUrl = req.protocol + '://' + req.get('host') ;
+            let appUrl = req.protocol + '://' + req.get('host');
             var id = req.params.id;
-            Driver.update({ _id: id }, { img: appUrl+"/uploads/"+id }).then(driver => {
+            Driver.update({ _id: id }, { img: appUrl + "/uploads/" + id }).then(driver => {
                 res.status(204).end();
             });
 
@@ -193,11 +196,11 @@ module.exports = {
                 res.send({ msg: "No Drivers Near You" });
                 return;
             }
-         
+
             var nearestLocation = null;
             var driversLocations = [];
 
-            console.log(result) ;
+            console.log(result);
             result.map((r, index) => {
                 var id = r[0][0]
                 var longitude = r[1][0];
@@ -214,13 +217,13 @@ module.exports = {
                 uri: 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + nearestLocation.latitude + "," + nearestLocation.longitude + "&destinations=" + lat + "," + lng + "&departure_time=now" + "&key=AIzaSyDe_nAWE5ccLGXPuWbcGTXzVlrtH-lMcUw",
                 json: true
             };
-             
-            var destination = {
-                latitude:lat,
-                longitude:lng
-            } ;
 
-            getEta(nearestLocation,destination).then(result => {
+            var destination = {
+                latitude: lat,
+                longitude: lng
+            };
+
+            getEta(nearestLocation, destination).then(result => {
                 res.send({
                     drivers: driversLocations,
                     nearTime: result
@@ -232,53 +235,53 @@ module.exports = {
 
     },
 
-    findTrips(req, res){
+    findTrips(req, res) {
         let page = req.query.page;
         let limit = req.query.limit;
-       
+
         page = page ? parseInt(page) : 1;
         limit = limit ? limit : 30;
-       
-        let id = req.params.id ;
-        
+
+        let id = req.params.id;
+
         Driver.findById(id).then(driver => {
 
-            if(! driver){
-                res.status(404).send({ success:false , error: "Driver not found" }) ;
-                return ;
+            if (!driver) {
+                res.status(404).send({ success: false, error: "Driver not found" });
+                return;
             }
 
             let findQuery = Trip.find({ driver: id });
-            findQuery.populate('driver') ;
-            let countQuery = Trip.count({ driver: id  });
+            findQuery.populate('driver');
+            let countQuery = Trip.count({ driver: id });
 
             findQuery.sort({ endDate: -1 })
-            .limit(parseInt(limit))
-            .skip((page - 1) * limit).then(results => {
+                .limit(parseInt(limit))
+                .skip((page - 1) * limit).then(results => {
 
-                countQuery.then(count => {
+                    countQuery.then(count => {
 
-                    let pageCount = Math.ceil(count / limit);
+                        let pageCount = Math.ceil(count / limit);
 
-                    let response = new ApiResponse(results, page, pageCount, limit, count);
-                    response.addSelfLink(req);
+                        let response = new ApiResponse(results, page, pageCount, limit, count);
+                        response.addSelfLink(req);
 
-                    if (page > 1) {
-                        response.addPrevLink(req);
-                    }
-                    if (page < pageCount) {
-                        response.addNextLink(req);
-                    }
-            
-                    res.send(response);
-                }).catch(err => console.log(err))
-                
-            }).catch(err => console.log(err) );
+                        if (page > 1) {
+                            response.addPrevLink(req);
+                        }
+                        if (page < pageCount) {
+                            response.addNextLink(req);
+                        }
+
+                        res.send(response);
+                    }).catch(err => console.log(err))
+
+                }).catch(err => console.log(err));
         }).catch(err => {
-            res.status(404).send({ success:false , error: "Driver not found" }) ;
+            res.status(404).send({ success: false, error: "Driver not found" });
         })
     },
-    
+
 }
 
 
